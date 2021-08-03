c4d65e0dd0981074c9e37884df0be6a22d847a12
Jess2D <jessicap_silveira@hotmail.com>
2021-08-03 20:46:45 +1200

    Add card format

--- dist/main.js
+++ dist/main.js
@@ -195,7 +195,42 @@ function nav(main) {
     grid.classList.add("grid")
     wrap.appendChild(grid)
 
-  
+  let elements = [
+      {"class":"food-photo", 
+      "src" : "../img/chia.jpg",
+      "alt": "Chia", 
+      "h2": "Chia Pudding"}, 
+
+      {"class":"food-photo", 
+      "src" : "../img/fetta-salad.jpg",
+      "alt": "Fetta Salad", 
+      "h2": "Fetta Salad"}, 
+
+
+      {"class":"food-photo", 
+      "src" : "../img/chiken-salad.jpg",
+      "alt": "Chicken Salad", 
+      "h2": "Chicken Salad"}
+
+  ]
+
+  elements.map(elemnt => {
+      let card = document.createElement("div")
+      card.classList.add("card")
+      grid.appendChild(card)
+
+      let image = document.createElement("img")
+      image.setAttribute("src", elemnt.src)
+      image.setAttribute("alt", elemnt.alt)
+      image.classList.add(elemnt.class)
+      card.appendChild(image)
+
+      let h2 = document.createElement("h2")
+      h2.innerText = elemnt.h2
+      card.appendChild(h2)
+      grid.appendChild(card)
+      
+  })
 
     
 }
